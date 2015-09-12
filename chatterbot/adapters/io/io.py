d2374ade24cc70caf9554663c084ed6768edf598
Gunther Cox <gunthercx@gmail.com>
2015-09-12 19:23:26 -0400

    Kwargs are now passed to io adapter.

--- chatterbot/adapters/io/io.py
+++ chatterbot/adapters/io/io.py
@@ -3,6 +3,15 @@ from chatterbot.adapters.exceptions import AdapterNotImplementedError
 
 class IOAdapter(object):
 
+    def __init__(self, **kwargs):
+        pass
+
+    def process_input(self):
+        """
+        Returns data retrieved from the input source.
+        """
+        raise AdapterNotImplementedError()
+
     def process_response(self, input_value):
         """
         Takes an input value.
